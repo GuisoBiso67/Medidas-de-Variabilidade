4b50c5ec20f2e883546d37d3b145661bded5770d
Guilherme <guisobiso@gmail.com>
2022-08-23 19:14:23 -0300

    adicionei variancia e desvio padrao

--- medidasDeVariabilidade.cpp
+++ medidasDeVariabilidade.cpp
@@ -5,6 +5,7 @@ int main(){
     int nValores;
     double maior, menor; // variaveis 'amplitude'
     double somaDesvios=0, mediaDesvios=0, temp=0; // variaveis 'desvios'
+    double quadradosDaDiferença=0, mediaQuadradosDaDiferença;
     cout << "Insira a quantidade de valores: ";
     cin >> nValores;
 
@@ -34,23 +35,30 @@ int main(){
     cout << "Amplitude = " << fixed << setprecision(3) << maior-menor << endl;
 
     // DESVIOS
+    cout << endl;
     cout << "/ VALUE / DIFERENCAS;DESVIOS /" << endl;
     for (int i=0; i<nValores; i++){
         cout << "/ " << fixed << setprecision(2) << valores[i] << " / " << fixed << setprecision(2) << valores[i] << " - " << fixed << setprecision(2) << media << " = " << fixed << setprecision(2) << valores[i] - media << " / " << endl;
 
-    // DESVIO MÉDIO
+    // DESVIO MÉDIO & QUADRADOS DAS DIFERENÇAS
         temp = (valores[i] - media);
         if (temp < 0){
             somaDesvios += (temp*-1);
+            quadradosDaDiferença += (temp*-1)*(temp*-1);
         }else{
             somaDesvios += temp;
+            quadradosDaDiferença += (temp*temp);
+       
         }
     }
+    cout << endl;
     mediaDesvios = (somaDesvios/nValores);
+    mediaQuadradosDaDiferença = (quadradosDaDiferença/nValores);
     cout << "Desvio Medio (DM) = " << fixed << setprecision(3) << mediaDesvios << endl;
 
     // VARIÂNCIA
+    cout << "Variancia (Var) = " << fixed << setprecision(3) << mediaQuadradosDaDiferença << endl;
 
     // DESVIO PADRÃO
-
+    cout << "Desvio Padrao (DP) = " << fixed << setprecision(3) << sqrt(mediaQuadradosDaDiferença) << endl;
 }
